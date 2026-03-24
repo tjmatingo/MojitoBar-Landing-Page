af10d5200403c69a5ecafb76a1f603df07ba1068
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-24 22:33:04 +0200

    navbar setup with constants folder

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+export const navLinks = [
+    {id: 'cocktails', title: "Cocktails"},
+    {id: 'about', title: "About Us"},
+
+]
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 
+
+// components
+import Navbar from "./components/Navbar";
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center bg-slate-900 p-5 h-[100vh]">
-      <h1 className="text-3xl text-rose-200">Mojito Cocktails</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 }
  
