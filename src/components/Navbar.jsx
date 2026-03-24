2541d761240e3b71293d67e7b369f76f35802beb
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-24 22:35:54 +0200

    constants setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import {navLinks} from '../../constants/index.js'
 
+// ffmpeg -i input.mp4 -vf scale=960:-1 -movflags faststart -vcodec libx264 -crf 20 -g 1 -pix_fmt yuv420p output.mp4
+
+
 const Navbar = () => {
     return (
         <nav>
