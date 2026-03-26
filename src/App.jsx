2b26dac8779ab15bbbb3b343857ee677bf23451d
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-26 19:51:44 +0200

    initialized css styling in leaves

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import gsap from "gsap";
 // components
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/HeroSect.jsx";
+import Cocktails from "./components/Cocktails.jsx"
  
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -12,6 +13,7 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
+      <Cocktails />
       {/* <div className="h-dvh bg-black"></div> */}
     </main>
   );
