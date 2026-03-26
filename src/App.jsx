1bf90ff58e7752bc072736f7b3ce80c06c2f0bd4
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-26 19:28:43 +0200

    smoother video runnig

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
-      <div className="h-dvh bg-black"></div>
+      {/* <div className="h-dvh bg-black"></div> */}
     </main>
   );
 }
